b077b2279b1d54a6f6e1d5781a179a0afada3b1e
曹发友(80016865) <fayou.cao@hxgroup.com>
2025-01-13 17:25:01 +0800

    代理服务器配置

--- chatbot-project/src/components/ChatMessage.jsx
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -1,9 +1,11 @@
 import RobotProfileImage from '../assets/robot.png';
-import UserProfileImage from '../assets/user.png';
+// import UserProfileImage from '../assets/user.png';
 import './ChatMessage.css';
 
 export function ChatMessage(props) {
   const { message, sender } = props;
+  const UserProfileImage = '/proxy/images/profile-1.jpg';
+  console.log(UserProfileImage);
   return (
     <div
       className={sender === 'user' ? 'chat-message-user' : 'chat-message-robot'}
