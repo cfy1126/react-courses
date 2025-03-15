d9934dbd6ca4ac970dea5b08348f083462b995a3
caofayou <2017954256@qq.com>
2025-03-15 15:22:06 +0800

    增加404页面

---
+++ ecommerce-project/src/pages/404.jsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header';
+
+export function NotFoundPage() {
+  return (
+    <>
+      <Header />
+      <div style={{ marginTop: 100 }}>404 page</div>
+    </>
+  );
+}
